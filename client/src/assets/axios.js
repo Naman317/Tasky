a3c624b6cffde023df4f43796c782191aa560c05
unknown <namansharma3194@gmail.com>
2026-05-03 00:41:54 +0530

    .

--- client/src/assets/axios.js
+++ client/src/assets/axios.js
@@ -2,10 +2,14 @@ import axios from "axios";
 import store from "../redux/store";
 import { logoutUser } from "../redux/slices/authSlice";
 
+const API_URL = import.meta.env.VITE_APP_BASE_URL || "http://localhost:5000";
+
+
 const API = axios.create({
-  baseURL: import.meta.env.VITE_APP_BASE_URL + "/api" || "http://localhost:5055/api",
+  baseURL: `${API_URL}/api`,
   withCredentials: true,
 });
+
 
 API.interceptors.response.use(
   (response) => response,
